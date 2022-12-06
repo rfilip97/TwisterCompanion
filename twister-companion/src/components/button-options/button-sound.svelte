4e4587206f7000e65217052700f2e6ed81467205
Razvan Filip <filip.razvan@agilefreaks.com>
2022-12-06 12:03:37 +0200

    Split svg element to separate components

--- twister-companion/src/components/button-options/button-sound.svelte
+++ twister-companion/src/components/button-options/button-sound.svelte
@@ -1,20 +1,13 @@
 <script>
 	import ButtonOptions from './button-options.svelte';
+	import SvgContainer from './svg-container.svelte';
+	import SvgPath from './svg-path.svelte';
 </script>
 
 <ButtonOptions>
-	<svg
-		class="w-6 h-6"
-		fill="none"
-		stroke="currentColor"
-		viewBox="0 0 24 24"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			stroke-width="2"
-			d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"
+	<SvgContainer>
+		<SvgPath
+			model="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"
 		/>
-	</svg>
+	</SvgContainer>
 </ButtonOptions>
---
+++ twister-companion/src/components/button-options/svg-path.svelte
@@ -0,0 +1,5 @@
+<script>
+	export let model;
+</script>
+
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d={model} />
