ae2edb8a0224a6578bd38ed6afd94bc2974334ba
Razvan Filip <filip.razvan@agilefreaks.com>
2022-12-06 11:53:09 +0200

    Move buttons to own classes

--- twister-companion/src/routes/+layout.svelte
+++ twister-companion/src/routes/+layout.svelte
@@ -1,45 +1,12 @@
 <script>
 	import '../app.postcss';
-	import ButtonOptions from '../components/button-options/button-options.svelte';
+	import ButtonSettings from '../components/button-options/button-settings.svelte';
+	import ButtonSound from '../components/button-options/button-sound.svelte';
 </script>
 
 <div class="header flex flex-row justify-end gap-1 mr-4 mt-4">
-	<ButtonOptions>
-		<svg
-			class="w-6 h-6"
-			fill="none"
-			stroke="currentColor"
-			viewBox="0 0 24 24"
-			xmlns="http://www.w3.org/2000/svg"
-			><path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"
-			/></svg
-		>
-	</ButtonOptions>
-
-	<ButtonOptions>
-		<svg
-			class="w-6 h-6"
-			fill="none"
-			stroke="currentColor"
-			viewBox="0 0 24 24"
-			xmlns="http://www.w3.org/2000/svg"
-			><path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-			/><path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-			/></svg
-		>
-	</ButtonOptions>
+	<ButtonSound />
+	<ButtonSettings />
 </div>
 
 <slot />
