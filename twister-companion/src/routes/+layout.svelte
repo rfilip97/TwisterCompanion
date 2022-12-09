9141f072d4129d35fe4f5f4466d46451c2bbf811
Razvan Filip <filip.razvan@agilefreaks.com>
2022-12-09 13:18:53 +0200

    Add conditional render for sound icon

--- twister-companion/src/routes/+layout.svelte
+++ twister-companion/src/routes/+layout.svelte
@@ -3,11 +3,17 @@
 	import ButtonSound from '../components/button-options/button-sound.svelte';
 	import ButtonMuted from '../components/button-options/button-muted.svelte';
 	import ModalSettings from '../components/modals/modal-settings.svelte';
+
+	import { config } from '../stores/config-store';
 </script>
 
 <div class="header flex flex-row justify-end gap-3 mr-4 mt-4">
-	<ButtonSound />
-	<ButtonMuted />
+	{#if $config.muted}
+		<ButtonMuted />
+	{:else}
+		<ButtonSound />
+	{/if}
+
 	<ModalSettings />
 </div>
 
