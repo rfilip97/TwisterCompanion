a03de8c1f76fee2209776d76d259d8ec69bc6683
Razvan Filip <filip.razvan@agilefreaks.com>
2022-12-09 13:07:26 +0200

    Add muted button

--- twister-companion/src/routes/+layout.svelte
+++ twister-companion/src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import '../app.postcss';
 	import ButtonSound from '../components/button-options/button-sound.svelte';
+	import ButtonMuted from '../components/button-options/button-muted.svelte';
 	import ModalSettings from '../components/modals/modal-settings.svelte';
 </script>
 
 <div class="header flex flex-row justify-end gap-3 mr-4 mt-4">
 	<ButtonSound />
+	<ButtonMuted />
 	<ModalSettings />
 </div>
 
