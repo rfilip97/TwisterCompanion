6fcfdd36c247625dfca4070739b20b06f7d326a6
Razvan Filip <filip.razvan@agilefreaks.com>
2022-12-11 15:18:19 +0200

    Add menu routes

--- twister-companion/src/components/menu/menu.svelte
+++ twister-companion/src/components/menu/menu.svelte
@@ -20,24 +20,14 @@
 			id="drawer-label"
 			class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"
 		>
-			<svg
-				class="w-5 h-5 mr-2"
-				aria-hidden="true"
-				fill="currentColor"
-				viewBox="0 0 20 20"
-				xmlns="http://www.w3.org/2000/svg"
-				><path
-					fill-rule="evenodd"
-					d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-					clip-rule="evenodd"
-				/>
-			</svg>
 			Menu
 		</h5>
 		<CloseButton on:click={() => (hidden1 = true)} class="mb-4 dark:text-white" />
 	</div>
 	<div>
-		<p>Item #1</p>
-		<p>Item #2</p>
+		<div class="flex flex-col">
+			<a href="/">Play</a>
+			<a href="/about">About</a>
+		</div>
 	</div>
 </Drawer>
---
+++ twister-companion/src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<div>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate accusantium cupiditate
+		eligendi quos hic nostrum architecto saepe earum, quia aliquam totam dolor deleniti voluptatum
+		quae assumenda laborum ratione ipsa vel mollitia facere iste. Necessitatibus facilis nesciunt,
+		reprehenderit, ex veritatis laborum sed animi nostrum magni rerum eveniet illum consequatur
+		dignissimos quod?
+	</p>
+</div>
