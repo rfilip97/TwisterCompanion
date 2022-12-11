d8c618ac9828e63c8eb8ddf6270cc1384f021dcf
Razvan Filip <filip.razvan@agilefreaks.com>
2022-12-11 16:48:02 +0200

    Move menu cards to separate components

--- twister-companion/src/components/menu/menu.svelte
+++ twister-companion/src/components/menu/menu.svelte
@@ -2,6 +2,9 @@
 	import { Drawer, Button, Card, CloseButton } from 'flowbite-svelte';
 	import { sineIn } from 'svelte/easing';
 
+	import CardAbout from './card-about.svelte';
+	import CardPlay from './card-play.svelte';
+
 	let hidden1 = true;
 	let transitionParams = {
 		x: -320,
@@ -42,45 +45,8 @@
 	<div>
 		<div class="flex flex-col">
 			<div>
-				<Card href="/" horizontal>
-					<svg
-						class="w-6 h-6"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-						/>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-						/>
-					</svg>
-					<p class="ml-2">Play</p>
-				</Card>
-
-				<Card href="/about" horizontal>
-					<svg
-						class="w-6 h-6"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-						/></svg
-					>
-					<p class="ml-2">About</p>
-				</Card>
+				<CardPlay />
+				<CardAbout />
 			</div>
 		</div>
 	</div>
