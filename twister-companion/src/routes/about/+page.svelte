bb96bab20cb4a3edea2760552f4cb1eee0bdf0b7
Razvan Filip <filip.razvan@agilefreaks.com>
2022-12-11 16:59:11 +0200

    Use typography component instead of plain paragraph for about page

--- twister-companion/src/routes/about/+page.svelte
+++ twister-companion/src/routes/about/+page.svelte
@@ -1,9 +1,13 @@
+<script>
+	import { P } from 'flowbite-svelte';
+</script>
+
 <div>
-	<p>
+	<P class="m-10" weight="light" color="text-gray-500">
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate accusantium cupiditate
 		eligendi quos hic nostrum architecto saepe earum, quia aliquam totam dolor deleniti voluptatum
 		quae assumenda laborum ratione ipsa vel mollitia facere iste. Necessitatibus facilis nesciunt,
 		reprehenderit, ex veritatis laborum sed animi nostrum magni rerum eveniet illum consequatur
 		dignissimos quod?
-	</p>
+	</P>
 </div>
