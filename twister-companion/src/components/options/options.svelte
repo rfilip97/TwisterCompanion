47083cb522607acb3ba3eba612e85a8ba7604cb9
Razvan Filip <filip.razvan@agilefreaks.com>
2022-12-11 17:03:10 +0200

    Move options to separate component

---
+++ twister-companion/src/components/options/options.svelte
@@ -0,0 +1,17 @@
+<script>
+	import ButtonSound from '../../components/button-options/button-sound.svelte';
+	import ButtonMuted from '../../components/button-options/button-muted.svelte';
+	import ModalSettings from '../../components/modals/modal-settings.svelte';
+
+	import { config } from '../../stores/config-store';
+</script>
+
+<div class="header flex flex-row justify-end gap-3 mr-4 mt-4">
+	{#if $config.muted}
+		<ButtonMuted onClick={config.toggleMuted} />
+	{:else}
+		<ButtonSound onClick={config.toggleMuted} />
+	{/if}
+
+	<ModalSettings />
+</div>
--- twister-companion/src/routes/+layout.svelte
+++ twister-companion/src/routes/+layout.svelte
@@ -1,22 +1,11 @@
 <script>
 	import '../app.postcss';
-	import ButtonSound from '../components/button-options/button-sound.svelte';
-	import ButtonMuted from '../components/button-options/button-muted.svelte';
-	import ModalSettings from '../components/modals/modal-settings.svelte';
 
-	import { config } from '../stores/config-store';
 	import Menu from '../components/menu/menu.svelte';
+	import Options from '../components/options/options.svelte';
 </script>
 
 <Menu />
-<div class="header flex flex-row justify-end gap-3 mr-4 mt-4">
-	{#if $config.muted}
-		<ButtonMuted onClick={config.toggleMuted} />
-	{:else}
-		<ButtonSound onClick={config.toggleMuted} />
-	{/if}
-
-	<ModalSettings />
-</div>
+<Options />
 
 <slot />
